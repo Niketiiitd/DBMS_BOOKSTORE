2dbd8895b3ee2dc354c0152a7a6f85d2c78cae02
Niketiiitd <niket22320@iiitd.ac.in>
2024-03-27 19:34:24 +0530

    login page styled and 4 pages made

--- webapp/flaskblog.py
+++ webapp/flaskblog.py
@@ -1,28 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Dummy data for demonstration
+users = {
+    'admin': {'password': 'adminpass'},
+    'customer': {'password': 'customerpass'},
+    'vendor': {'password': 'vendorpass'}
+}
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template('index.html')
+def index():
+    return "Welcome to the main page"
 
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login_signup.html')
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user_type = request.form['user_type']
 
+        if username in users and users[username]['password'] == password:
+            # Successful login, redirect to respective homepage
+            if user_type == 'admin':
+                return redirect(url_for('admin_home'))
+            elif user_type == 'customer':
+                return redirect(url_for('customer_home'))
+            elif user_type == 'vendor':
+                return redirect(url_for('vendor_home'))
+            else:
+                return "Invalid user type"
+        else:
+            return "Invalid username or password"
+    return render_template('login.html')
 
-@app.route("/signup")
-def signup():
-    return render_template('login_signup.html')
+@app.route("/admin")
+def admin_home():
+    return "Welcome to the Admin Homepage"
 
+@app.route("/customer")
+def customer_home():
+    return "Welcome to the Customer Homepage"
+
+@app.route("/vendor")
+def vendor_home():
+    return "Welcome to the Vendor Homepage"
 
 if __name__ == '__main__':
     app.run(debug=True)
