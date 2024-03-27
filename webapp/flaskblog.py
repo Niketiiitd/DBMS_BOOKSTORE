df9ada74eae9ad0a768cdaac718f568618de9886
Niketiiitd <niket22320@iiitd.ac.in>
2024-03-27 20:27:50 +0530

    login page created

--- webapp/flaskblog.py
+++ webapp/flaskblog.py
@@ -1,50 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Dummy data for demonstration
-users = {
-    'admin': {'password': 'adminpass'},
-    'customer': {'password': 'customerpass'},
-    'vendor': {'password': 'vendorpass'}
-}
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
-@app.route("/")
-def index():
-    return "Welcome to the main page"
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/', methods=['POST'])
 def login():
-    if request.method == "POST":
-        username = request.form['username']
+    user_type = request.form['user_type']
+    if user_type == 'customer':
+        username = request.form['customer_id']
         password = request.form['password']
-        user_type = request.form['user_type']
-
-        if username in users and users[username]['password'] == password:
-            # Successful login, redirect to respective homepage
-            if user_type == 'admin':
-                return redirect(url_for('admin_home'))
-            elif user_type == 'customer':
-                return redirect(url_for('customer_home'))
-            elif user_type == 'vendor':
-                return redirect(url_for('vendor_home'))
-            else:
-                return "Invalid user type"
+        customer = validate_customer(username, password)
+        if customer:
+            return redirect(url_for('customer_dashboard'))
         else:
-            return "Invalid username or password"
-    return render_template('login.html')
-
-@app.route("/admin")
-def admin_home():
-    return "Welcome to the Admin Homepage"
-
-@app.route("/customer")
-def customer_home():
-    return "Welcome to the Customer Homepage"
-
-@app.route("/vendor")
-def vendor_home():
-    return "Welcome to the Vendor Homepage"
+            flash('Invalid username or password')
+    elif user_type == 'supplier':
+        return redirect(url_for('supplier_dashboard'))
+    else:
+        return "Invalid user type"
 
 if __name__ == '__main__':
     app.run(debug=True)
